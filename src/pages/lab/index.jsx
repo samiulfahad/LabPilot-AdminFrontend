aa81d0201d86e47c6d7f28834c9e718eea87df21
samiulfahad <samiul_fahad@yahoo.com>
2025-11-22 01:19:26 +0600

    Lab Page updated

--- src/pages/lab/index.jsx
+++ src/pages/lab/index.jsx
@@ -130,16 +130,21 @@ const Labs = () => {
         />
       )}
 
-      <div className="flex items-center justify-center mt-4 my-2">
+      <div className="flex items-center justify-center mb-4">
         <button
           onClick={() => {
             setFormData({ ...initialData, type: "addLab" });
             setIsModalOpen(true);
             loadZones();
           }}
-          className="btn-md w-60 mx-auto"
+          className="add-btn"
         >
-          Add new Lab
+          <div className="flex items-center justify-center gap-2">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+            </svg>
+            Add New Lab
+          </div>
         </button>
       </div>
 
