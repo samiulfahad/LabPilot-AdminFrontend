baa6aef9a599af8a345c2d99a4c49ecdd9032b47
samiulfahad <samiul_fahad@yahoo.com>
2025-12-06 00:36:37 +0600

    Layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
 
       {/* Main content - Adjusted for simpler sidebar */}
       <main className="lg:ml-64 flex-1 min-h-screen bg-white">
-        <div className="space-y-8 pt-20 lg:pt-3 bg-white w-full max-w-full mx-auto p-4">{children}</div>
+        <div className="space-y-8 pt-20 lg:pt-3 bg-white w-full max-w-[100vw] mx-auto p-4">{children}</div>
       </main>
     </div>
   );
