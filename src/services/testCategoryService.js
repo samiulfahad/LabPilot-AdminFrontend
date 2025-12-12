de9e6ca1fb720a0ef89d2ddc9c6a51871ea786ab
samiulfahad <samiul_fahad@yahoo.com>
2025-12-13 03:25:05 +0600

    Testlist page dynamic reload done

--- src/services/testCategoryService.js
+++ src/services/testCategoryService.js
@@ -5,7 +5,6 @@ const testCategoryService = {
   editCategory: (data) => api.patch("/lab/testCategory/edit", data),
   deleteCategory: (categoryId) => api.delete(`/lab/testCategory/delete/${categoryId}`),
   getCategoryList: () => api.get("/lab/testCategory/all"),
-  getPopulatedList: () => api.get("/lab/testCategory/populate"),
 };
 
 export default testCategoryService;
